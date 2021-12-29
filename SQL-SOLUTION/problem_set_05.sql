df7a33ab23c72c4ba44e14ec01e4c04df626f43b
VijayKottaiyan K <68232132+VijayKottaiyan@users.noreply.github.com>
2021-12-29 12:42:11 +0530

    Update problem_set_05.sql

--- SQL-SOLUTION/problem_set_05.sql
+++ SQL-SOLUTION/problem_set_05.sql
@@ -1,5 +1,5 @@
 #1
-select a.name from musician a inner join concert b on a.m_no = b.concert_organiser 
+select a.m_name from musician a inner join concert b on a.m_no = b.concert_orgniser 
 where con_date > '1997-02-31' and b.concert_venue = "Assembly Rooms";
 
 #2
@@ -7,9 +7,9 @@ select a.* from musician a inner join performer b on a.m_no = b.perf_is inner jo
 where (b.instrument = "guitar" or b.instrument = "violin") and c.place_country = "England";
 
 #3
-select a.name, c.place_town, c.place_country, b.con_date from musician a inner join concert b on a.m_no = b.concert_organiser
+select a.m_name, c.place_town, c.place_country, b.con_date from musician a inner join concert b on a.m_no = b.concert_orgniser
 inner join place c on b.concert_in = c.place_no 
-where palce_country = "USA";
+where place_country = "USA";
 
 #4
 select con_date,c_title,concert_venue
@@ -33,3 +33,19 @@ select instrument from performer where perf_is = ( select m_no from musician whe
 );
 
 #7
+select band_name, no_of_players 
+from band inner join 
+(select band_id, count(player) as no_of_players from plays_in group by band_id) a 
+on band_no = band_id
+where no_of_players > (select avg(no_of_players) as avg_players from (select band_id, count(player) as no_of_players from plays_in group by band_id) b);
+
+#8
+select distinct musician.* from 
+musician join place on living_in = place_no
+join composer on m_no = comp_is
+join performance on m_no = conducted_by
+where place_country = "England";
+
+
+#9
+
